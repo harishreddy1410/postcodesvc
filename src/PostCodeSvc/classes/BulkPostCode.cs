79deb6ecca20b84b06b63c541bf6f68f468655ad
harishreddy1410 <harishreddy1410@gmail.com>
2022-07-18 16:06:23 +0530

    code cleanup

--- src/PostCodeSvc/classes/BulkPostCode.cs
+++ src/PostCodeSvc/classes/BulkPostCode.cs
@@ -4,6 +4,7 @@ public class BulkPostCode
 {
     [JsonPropertyName("query")]
     public string Query { get; set; }
+    
     [JsonPropertyName("result")]
     public PostCode Result { get; set; }
 }
--- src/PostCodeSvc/services/PostCodeService.cs
+++ src/PostCodeSvc/services/PostCodeService.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 using System.Net.Http.Headers;
 
 public class PostCodeService
-{
-    
+{    
     private static readonly HttpClient client = new HttpClient();
+    private readonly postCodeApiUrl = "https://api.postcodes.io/postcodes/";
     public async Task<PostCode> PostCodeLookup(string postCode)
     {
-        string url = "https://api.postcodes.io/postcodes/" + postCode;        
+        string url = postCodeApiUrl + postCode;        
         var result = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url));
         var jsonData = JsonSerializer.Deserialize<Dictionary<string, object>>
         (await result.Content.ReadAsStringAsync());
@@ -19,30 +19,24 @@ public class PostCodeService
 
     public async Task<List<PostCode>> AutoComplete(string postCode)
     {
-        string url = "https://api.postcodes.io/postcodes/" + postCode + "/autocomplete";        
+        string url = postCodeApiUrl + postCode + "/autocomplete";        
         var result = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url));
         var jsonData = JsonSerializer.Deserialize<Dictionary<string, object>>(await result.Content.ReadAsStringAsync());
         List<PostCode> searchResult = new List<PostCode>();
         if(jsonData["result"] != null)
         {
             //Make bulk lookup
-            url = "https://api.postcodes.io/postcodes";
             var payload = "{\"postcodes\":"+ jsonData["result"].ToString() + "}";
             HttpContent content = new StringContent(payload, System.Text.Encoding.UTF8, "application/json");
-            var resp = await client.PostAsync(url,content);
+            var resp = await client.PostAsync(postCodeApiUrl,content);
             var bulkJson = JsonSerializer.Deserialize<Dictionary<string, object>>(await resp.Content.ReadAsStringAsync());
             if(bulkJson.ContainsKey("result"))
             {
                 System.Console.WriteLine("-----------PARSING BULK UPLOADS------");
-                //System.Console.WriteLine(bulkJson["result"]);
                 var finalResult = JsonSerializer.Deserialize<List<BulkPostCode>>(bulkJson["result"].ToString());
                 finalResult.ForEach(x=>{
                     searchResult.Add(x.Result);
                 });
-                //searchResult = resp.Select(x=>x.Result).ToList();
-                // System.Console.WriteLine(JsonSerializer.Serialize(finalRes));
-                // System.Console.WriteLine("TOTAL RECORDS " + finalRes.Count);
-                
             }
         }        
         return searchResult;
